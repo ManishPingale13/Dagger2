168713e3e959bc1329a85eecfe3bf4914c5a790f
Manish Pingale <manishpingale2005@gmail.com>
2021-01-14 16:37:28 +0530

    Field Injection..

--- app/src/main/java/com/example/dagger2/MainActivity.kt
+++ app/src/main/java/com/example/dagger2/MainActivity.kt
@@ -2,18 +2,19 @@ package com.example.dagger2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var car: Car
+    @Inject
+    lateinit var car: Car
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val carComponent = DaggerCarComponent.create()
-
-        car = carComponent.getCar()
+        carComponent.inject(this)
         car.drive()
 
     }
